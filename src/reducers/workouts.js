75878110d9ed577c63a5dcb7ba968397107de2f1
Alex Dovenmuehle <adovenmuehle@gmail.com>
2017-11-02 21:06:49 -0400

    Do stuff maybe not really sure

--- src/reducers/workouts.js
+++ src/reducers/workouts.js
@@ -2,7 +2,8 @@ const initState = [
     {id: 1, name: 'P90MisutoWolf', exercises: [
       {id: 1, date: new Date(2017, 10, 1), sets: [
         { id: 1, reps: 10, weight: 45 },
-        { id: 2, reps: 20, weight: 55 }
+        { id: 2, reps: 20, weight: 55 },
+        { workoutId: 1, exerciseId: 1, id: 2, reps: 30, weight: 55 }
       ]}
     ]},
     {id: 2, name: 'EnduroMax', exercises: []},
@@ -10,7 +11,13 @@ const initState = [
 
 const workouts = (state = initState, action) => {
     switch (action.type) {
-      case 'ADD_WORKOUT':
+      case 'ADD_SET':
+        let payload = action.payload;
+        let newState = {...state};
+        newState[action.workoutId][action.exerciseId].push(
+          payload
+        )
+        return newState;
         return [
           ...state,
           {
